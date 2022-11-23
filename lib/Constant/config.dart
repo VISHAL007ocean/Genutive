b3c4fa32c2a5e75a0f3e50d9794d9763b032fb66
oceanbtz <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>
2022-11-23 18:43:13 +0530

    config.dart

---
+++ lib/Constant/config.dart
@@ -0,0 +1,5 @@
+class AppConfig {
+  static String ApiUrl = "http://192.168.0.105:4998";
+  // static String ApiUrl = "http://192.168.0.105:4998";
+static String ClientXHeader = "@CLIENT_X_HEADER\$G%N&+!^";
+}
